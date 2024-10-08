9d52e1785edc25ef6a98327e66f632f963dd32dc
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-08 17:28:01 +0530

    Update people-list.component.ts

--- src/app/people/people-list/people-list.component.ts
+++ src/app/people/people-list/people-list.component.ts
@@ -1,1 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from '../people.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent implements OnInit {
+  people: any[] = [];
+
+  constructor(private peopleService: PeopleService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadPeople();
+  }
+
+  loadPeople(): void {
+    this.peopleService.getPeople().subscribe((data: any[]) => {
+      this.people = data;
+    });
+  }
+
+  editPerson(id: number): void {
+    this.router.navigate(['/edit-person', id]);
+  }
+
+  deletePerson(id: number): void {
+    if (confirm('Are you sure you want to delete this person?')) {
+      this.peopleService.deletePerson(id).subscribe(() => {
+        this.loadPeople(); // Reload the list after deletion
+      });
+    }
+  }
+}
 
