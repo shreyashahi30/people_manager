f0ff622166eeda1c833833674692d74ec42ced54
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-08 17:47:23 +0530

    Update auth.guard.ts

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from '../auth/auth.service'; // Adjust the path according to your project structure
+import { AuthService } from '../auth/auth.service'; 
 
 @Injectable({
   providedIn: 'root',
