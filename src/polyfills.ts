5c1ee70d891fd409ed1cf712af9842c09a901d5b
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-08 17:53:27 +0530

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,1 +1,12 @@
+/******************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Zone JS is required by Angular itself.
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+(window as any).__Zone_disable_ErrorStack = true; // Disable ErrorStack
+(window as any).__Zone_enable_cross_context_check = true; // Enable cross-context check
+
+// Other polyfills can be added below.
 
